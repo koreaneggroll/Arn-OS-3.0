b18cd1f83bf021ace80a838cbf9ef597c483a38e
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-02-01 15:05:16 +0200

    Final touch

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -3,5 +3,5 @@
 void kernel_main(){
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("If this works i'ma jump out the window bro");
+    print_str("Daca merge ma arunc pe geam");
 }
